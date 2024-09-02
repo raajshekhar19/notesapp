22eed26a4c6f7ae5fa8bb0bac17e938e9015b58f
raajshekhar19 <raajshekharbgp@gmail.com>
2024-09-02 20:38:37 +0530

    input bars added

--- notesapp/src/pages/Home/index.jsx
+++ notesapp/src/pages/Home/index.jsx
@@ -1,15 +1,32 @@
 import { Navbar } from "../../components/Navbar";
 import { Sidebar } from "../../components/Sidebar";
+import { useState } from "react";
 
 export const Home = () => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
   return (
     <>
       <Navbar />
       <main className="flex gap-3">
         <Sidebar />
-        <div>
-          <input placeholder="Enter the Title" />
-          <textarea placeholder="Enter the Text" />
+        <div className="flex flex-col w-[300px] h-[70px] border border-red-500 relative">
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border"
+            placeholder="Enter the Title"
+          />
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="border"
+            placeholder="Enter the Text"
+          />
+          <button className="absolute bottom-0 right-0">
+            <span class="material-symbols-outlined">add</span>
+          </button>
         </div>
       </main>
     </>
