bd6f19ddece82eeeed982d5a4f99814c880c6bd7
raajshekhar19 <raajshekharbgp@gmail.com>
2024-09-04 00:03:25 +0530

    reducer logic made

--- notesapp/src/pages/Home/index.jsx
+++ notesapp/src/pages/Home/index.jsx
@@ -1,10 +1,42 @@
 import { Navbar } from "../../components/Navbar";
 import { Sidebar } from "../../components/Sidebar";
-import { useState } from "react";
+import { useReducer, useState } from "react";
+import { notesReducer } from "../../reducers/notesreducer";
 
 export const Home = () => {
-  const [title, setTitle] = useState("");
-  const [text, setText] = useState("");
+  
+  const initialState = {
+    title : "",
+    text : "",
+    notes : []
+
+  }
+  const [{title,text,notes}, notesDispatch] = useReducer(notesReducer,initialState);
+
+  const onTitleChange =(e)=>{
+    notesDispatch({
+      type : 'TITLE',
+      payload : e.target.value
+    })
+  }
+
+  const onTextChange =(e)=>{
+    notesDispatch({
+      type : 'TEXT',
+      payload : e.target.value
+    })
+  }
+
+  const onClickAdd = ()=>{
+    notesDispatch({
+      type : 'ADD_NOTE'
+    })
+    notesDispatch({
+      type : 'CLEAR'
+    })
+      
+  }
+
 
   return (
     <>
@@ -14,17 +46,20 @@ export const Home = () => {
         <div className="flex flex-col w-[300px] h-[70px] border border-red-500 relative">
           <input
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={onTitleChange}
             className="border"
             placeholder="Enter the Title"
           />
           <textarea
             value={text}
-            onChange={(e) => setText(e.target.value)}
+            onChange={onTextChange}
             className="border"
             placeholder="Enter the Text"
           />
-          <button className="absolute bottom-0 right-0">
+          <button 
+          disabled = {title.length ===0 }
+          onClick={onClickAdd}
+          className="absolute bottom-0 right-0">
             <span class="material-symbols-outlined">add</span>
           </button>
         </div>
